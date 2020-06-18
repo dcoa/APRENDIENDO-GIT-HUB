2d79b5e1251d1a078da73946dae3cae67a657a5d
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-18 09:42:41 -0500

    prueba04

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-const a = 5;
+
 const title = document.querrySelectorAll("h1");
 
+   const b = 5;
+   let array = [1,2,3,4,5,6,7,8,9]
+
+   for(i=1; i<array.length;i++){
+    tabla =x*i;
+    console.log("tabla del cinco "+tabla)
+}
  
 
